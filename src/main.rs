8463e7ea01dfe2ac6c997d4d6d0db80aeb2c29b6
Nicholas Guarino <ndguarino@cadmia.io>
2021-03-21 15:51:12 -0400

    Remove dotenv

--- src/main.rs
+++ src/main.rs
@@ -65,8 +65,6 @@ impl EventHandler for DiscordHandler {
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    dotenv::dotenv()?;
-
     env_logger::init();
 
     let subscriber = FmtSubscriber::builder()
